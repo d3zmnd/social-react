280194dfa242a61d30c5c9484b52ab3f56505c1a
Vitaly Davidenko <d3zmnd@gmail.com>
2019-02-21 14:52:25 +0200

    Start creating SPA

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import Navbar from './components/navbar';
+import Profile from './components/profile';
+
+
+const App = () => {
+      return (
+        <div className='app-wrapper'>
+          <Header/>
+          <Navbar/>
+          <Profile/>
+        </div >);
+      }
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+ return <nav className='nav'>
+    <div>
+        <a>Profile</a>
+    </div>
+    <a> Massages</a>
+    <div>
+        <a>News</a>
+    </div>
+    <div>
+        <a>Settings</a>
+    </div>
+</nav>
+}
+export default Navbar;
